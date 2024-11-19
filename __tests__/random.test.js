42ff8174fff37ba422a98bb6b34a169afe3e4dfc
Master-Anku <ankumaster1@gmail.com>
2024-11-19 13:24:14 +0630

    Potion random test

---
+++ __tests__/random.test.js
@@ -0,0 +1,10 @@
+const randomNumber = require('../lib/random.js');
+
+
+test('gets random number between 1 and 10', () => {
+
+  expect(randomNumber()).toBeGreaterThanOrEqual(1);
+
+  expect(randomNumber()).toBeLessThanOrEqual(10);
+
+});
